7dafce3c24ace2ad028921c7db7475176bad55e2
GautamAnghore <gautamanghore@gmail.com>
2015-07-30 18:48:31 +0530

    redirection fix if user already logged in

--- apps/views.py
+++ apps/views.py
@@ -1,7 +1,12 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from apps import master
+from apps import Sessions
 
+sessions = Sessions()
 
 @master.route('/')
 def index():
+    if sessions.logged_in() is not None:
+        return redirect(url_for('dashboard.dashboard_index'))
+
     return render_template("home.html")
